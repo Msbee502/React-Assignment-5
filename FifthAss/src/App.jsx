3d40265913d157ff169a1f5fdabe326ea3ed4566
Msbee502 <b.hoogesteyn@gmail.com>
2024-12-03 15:23:46 +0100

    .

--- FifthAss/src/App.jsx
+++ FifthAss/src/App.jsx
@@ -2,6 +2,13 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import React from 'react';
 import Navbar from './pages/navbar.jsx';
 import { Link} from 'react-router-dom';
+import Home from './pages/home.jsx';
+import Images from './pages/images.jsx';
+import Maps from './pages/maps.jsx';
+import News from './pages/news.jsx';
+import Products from './pages/products.jsx';
+import SignIn from './pages/signIn.jsx';
+import Video from './pages/video.jsx';
 
 
 
@@ -12,7 +19,6 @@ function App () {
       <Navbar></Navbar>
       <Routes>
       <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
         <Route path="/video" element={<Video/>} />
         <Route path="/images" element={<Images/>} />
         <Route path="/maps" element={<Maps/>} />
